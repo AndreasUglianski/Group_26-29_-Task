eac1116bfcaef4d36b50845bfd169fb8622c5f97
AndreasUglianski <a.uglyansky@gmail.com>
2022-07-06 10:52:01 +0200

    changes added

--- HW4_buttons/scripts/script.js
+++ HW4_buttons/scripts/script.js
@@ -1,8 +1,37 @@
-// Cоздать кнопку с размерами 30 на 30 пикселей, при каждом нажатии увеличивать ее на 10 пикселей.
-// Cоздать кнопку, у которой при клике рандомно меняется цвет заднего фона
-// Создать генератор бросаемых костей (рандомно вычисляется два числа от 1 до 6 и выводятся в интерфейс).
-// Создать слайдер фотографий (при клике на фото оно должно меняться на одно из массива).
+//1.) Cоздать кнопку с размерами 30 на 30 пикселей, при каждом нажатии увеличивать ее на 10 пикселей.
 
+const incr_btn = document.querySelector('#incr_btn');
+let size = 30;
+incr_btn.addEventListener('click', () => {
+	size += 10;
+	incr_btn.style.width = size + 'px';
+	incr_btn.style.height = size + 'px';
+});
+
+// 3.) Создать генератор бросаемых костей (рандомно вычисляется два числа от 1 до 6 и выводятся в интерфейс).
+
+//2.) Cоздать кнопку, у которой при клике рандомно меняется цвет заднего фона
+
+// const change_color_btn = document.querySelector('.change_color_btn');
+// const colors = ['lightblue', 'darkgreen', 'red', 'blueviole', 'burlywood'];
+// let color_index = 0;
+
+// change_color_btn.addEventListener('click', () => {
+// 	let newColour = colors[Math.floor(Math.random() * colors.length)];
+// 	document.body.style.backgroundColor = colors[0];
+// 	colors.push(...colors.splice(0, 1));
+// });
+
+const change_color_btn = document.querySelector('.change_color_btn');
+function randomColor() {
+	return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+change_color_btn.addEventListener('click', () => {
+	document.body.style.backgroundColor = randomColor();
+});
+
+//4.) Создать слайдер фотографий (при клике на фото оно должно меняться на одно из массива).
+const photo_btn = document.querySelector('.photo_btn');
 const photos = [
 	'http://zhivotnue.ru/image/soxranenie_zhivotnux/soxranenie_barsuka/1.jpg',
 	'http://zhivotnue.ru/image/domashnie_zhivotnue/sobaki/nufaydlend/1.jpg',
@@ -10,14 +39,10 @@ const photos = [
 	'http://zhivotnue.ru/image/dicie_zhivotnue/loshadi/xeke/1.jpg',
 	'http://zhivotnue.ru/image/domashnie_zhivotnue/sobaki/bobbteil/1.jpg',
 ];
+let photo_index = 0;
 
-const change_color_btn = document.querySelector('.change_color_btn');
-const colors = ['lightblue', 'darkgreen', 'red', 'blueviole', 'burlywood'];
-let color_index = 0;
-
-change_color_btn.addEventListener(
-	'click',
-	() =>
-		(document.body.style.backgroundColor =
-			colors[color_index++ % colors.length])
-);
+photo_btn.addEventListener('click', () => {
+	let newPhoto = photos[Math.floor(Math.random() * photos.length)];
+	document.body.style.backgroundColor = photos[0];
+	photos.push(...photos.splice(0, 1));
+});
