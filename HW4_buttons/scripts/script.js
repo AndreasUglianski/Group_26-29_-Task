76f7a734d39ba8ceba1b6fb5f9e120e0948ba3d4
AndreasUglianski <a.uglyansky@gmail.com>
2022-07-07 08:07:23 +0200

    Last task added

--- HW4_buttons/scripts/script.js
+++ HW4_buttons/scripts/script.js
@@ -29,6 +29,24 @@ change_color_btn.addEventListener('click', () => {
 });
 // 3.) Создать генератор бросаемых костей (рандомно вычисляется два числа от 1 до 6 и выводятся в интерфейс).
 
+window.addEventListener('DOMContentLoaded', function () {
+	const buttonRoolDice = document.querySelector('.dice-roll');
+
+	function rollDice() {
+		const diceSide1 = document.getElementById('dice-side-1');
+		const diceSide2 = document.getElementById('dice-side-2');
+
+		const side1 = Math.floor(Math.random() * 6) + 1;
+		const side2 = Math.floor(Math.random() * 6) + 1;
+		const diceTotal = side1 + side2;
+
+		diceSide1.innerHTML = side1;
+		diceSide2.innerHTML = side2;
+	}
+
+	buttonRoolDice.addEventListener('click', rollDice);
+});
+
 // 4.) Создать слайдер фотографий (при клике на фото оно должно меняться на одно из массива).
 const slider = document.querySelector('.slider');
 const photo = document.querySelector('.photo');
